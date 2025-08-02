22d097dfbeed864c5e68fa571157a222124604ea
trung08012002 <phanngocanhyd@gmail.com>
2025-08-02 22:54:08 +0700

    feat: suggestion image clothes

---
+++ src/components/AsyncImageGenerator.tsx
@@ -0,0 +1,83 @@
+import { PlusOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { useState } from "react";
+
+export interface ImageGeneratorProps {
+  systemPrompt: string;
+}
+
+function ImageGenerator(props: ImageGeneratorProps) {
+  const { systemPrompt } = props;
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const generateImage = async () => {
+    setLoading(true);
+    const formData = new FormData();
+    const cleanPrompt = systemPrompt
+      .replace(/\s+/g, " ")
+      .replace(/,\s*$/, "")
+      .trim();
+    formData.append(
+      "prompt",
+      "A short-sleeved shirt with a bold pattern, paired with light-colored khaki shorts.Complete the look with dynamic white sneakers, ideal for comfortable outdoor activities. Use colors: #F08080, #FAEBD7, #FFF"
+    );
+    formData.append("output_format", "webp");
+
+    const response = await fetch(
+      "https://api.stability.ai/v2beta/stable-image/generate/core",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_STABILITY_KEY}`, // ⚠️ Không nên để API key ở frontend thật
+          Accept: "image/*",
+        },
+        body: formData,
+      }
+    );
+
+    if (response.ok) {
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setImageSrc(url);
+    } else {
+      const errorText = await response.text();
+      console.error("Error:", response.status, errorText);
+      alert("Image generation failed");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Button
+        type="primary"
+        icon={loading ? null : <PlusOutlined />}
+        style={{
+          marginTop: "1rem",
+          alignSelf: "end",
+          width: "100%",
+          height: "2.5rem",
+          backgroundColor: "#52c41a",
+          padding: "0 1rem",
+          borderColor: "#52c41a",
+          borderRadius: "0.5rem",
+          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.15)",
+        }}
+        loading={loading}
+        onClick={generateImage}
+      >
+        {loading ? "Generating..." : "Generate Image"}
+      </Button>
+      {imageSrc && (
+        <img
+          src={imageSrc}
+          alt="Generated"
+          style={{ marginTop: 20, maxWidth: "100%" }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default ImageGenerator;
--- src/pages/Mac.tsx
+++ src/pages/Mac.tsx
@@ -1,12 +1,12 @@
-import { Button, Card, Typography } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import { getFieldFromStorage, getUserData } from '../utils/localStorage';
-import { useEffect, useState } from 'react';
-import { generateGemini } from '../api/geminiService';
-import { geminiPromptTemplateMac } from '../templates/geminiPromptTemplateMac';
+import { Button, Card, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
+import { getFieldFromStorage, getUserData } from "../utils/localStorage";
+import { useEffect, useMemo, useState } from "react";
+import { generateGemini } from "../api/geminiService";
+import { geminiPromptTemplateMac } from "../templates/geminiPromptTemplateMac";
+import ImageGenerator from "../components/AsyncImageGenerator";
 
 const { Title, Text } = Typography;
-
 
 export default function Mac() {
   const navigate = useNavigate();
@@ -20,11 +20,16 @@ export default function Mac() {
       setLoading(true);
       try {
         const prompt = geminiPromptTemplateMac({
-          gender: getFieldFromStorage('moda_user_info', 'gender') ?? '',
-          birthdate: getFieldFromStorage('moda_user_info', 'dob') ?? '',
-          mbti: getFieldFromStorage('moda_user_info', 'mbti') ?? '',
-          temperature: getFieldFromStorage('moda_weather_data', 'data.weather.main.temp') ?? '',
-          weather: getFieldFromStorage('moda_weather_data', 'data.weather.weather.0.description') ?? '',
+          gender: getFieldFromStorage("moda_user_info", "gender") ?? "",
+          birthdate: getFieldFromStorage("moda_user_info", "dob") ?? "",
+          mbti: getFieldFromStorage("moda_user_info", "mbti") ?? "",
+          temperature:
+            getFieldFromStorage("moda_weather_data", "data.main.temp") ?? "",
+          weather:
+            getFieldFromStorage(
+              "moda_weather_data",
+              "data.weather.0.description"
+            ) ?? "",
         });
 
         const today = new Date().toISOString().slice(0, 10); // yyyy-mm-dd
@@ -51,11 +56,10 @@ export default function Mac() {
         if (suggestion) {
           setOutfit(suggestion);
         } else {
-          console.warn('Không có gợi ý hợp lệ.');
+          console.warn("Không có gợi ý hợp lệ.");
         }
-
       } catch (err) {
-        console.error('❌ Lỗi :', err);
+        console.error("❌ Lỗi :", err);
       } finally {
         setLoading(false);
       }
@@ -63,45 +67,79 @@ export default function Mac() {
 
     fetchData();
   }, []);
-
+  const prompt = useMemo(
+    () => [
+      `Create clothes which has to have colors ${
+        outfit?.colors?.length > 0 ? `with ${outfit.colors.join(", ")}` : null
+      }`,
+      outfit?.outfit_en,
+    ],
+    [outfit]
+  )
+    .filter(Boolean) // Loại bỏ null hoặc undefined
+    .join(", ");
   return (
     <div style={{ padding: 32 }}>
-      <Button onClick={() => navigate('/menu')} style={{ marginBottom: 16 }}>
+      <Button onClick={() => navigate("/menu")} style={{ marginBottom: 16 }}>
         ← Quay lại
       </Button>
-
-      <Title level={3}>Trang phục gợi ý cho {user.name} ({user.mbti})</Title>
-      <h2>Thời tiết:</h2>{' '}
-      <h2>{getFieldFromStorage('moda_weather_data', 'data.weather.weather.0.description')} - {getFieldFromStorage('moda_weather_data', 'data.weather.main.temp')}°C</h2>
-
+      <Title level={3}>
+        Trang phục gợi ý cho {user.name} ({user.mbti})
+      </Title>
+      <h2>Thời tiết:</h2>{" "}
+      <h2>
+        {getFieldFromStorage("moda_weather_data", "data.weather.0.description")}
+        , {getFieldFromStorage("moda_weather_data", "data.main.temp")}°C
+      </h2>
       {loading ? (
         <p>Đang lấy gợi ý ...</p>
-      ) : outfit && (
-        <Card title="Màu sắc tổng thể" style={{ maxWidth: 600, marginTop: 24, borderRadius: 8 }}>
-          {Array.isArray(outfit?.colors) && (
-            <div style={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: 180,
-              overflow: 'hidden',
-              marginBottom: 16,
-              gap: 8
-            }}>
-              {outfit.colors.map((color: string, idx: number) => (
-                <div key={idx} style={{
-                  width: '50%',
-                  height: 60,
-                  backgroundColor: color,
-                  border: '2px solid #666',
-                  borderRadius: 8
-                }}/>
-              ))}
-            </div>
-          )}
-          <h2><strong>Gợi ý:</strong> {outfit.outfit}</h2>
-          {outfit.accessories && <p><strong>Phụ kiện:</strong> {outfit.accessories}</p>}
-          {outfit.note && <p><em>{outfit.note}</em></p>}
-        </Card>
+      ) : (
+        outfit && (
+          <Card
+            title="Màu sắc tổng thể"
+            style={{ maxWidth: 600, marginTop: 24, borderRadius: 8 }}
+          >
+            {Array.isArray(outfit?.colors) && (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 180,
+                  overflow: "hidden",
+                  marginBottom: 16,
+                  gap: 8,
+                }}
+              >
+                {outfit.colors.map((color: string, idx: number) => (
+                  <div
+                    key={idx}
+                    style={{
+                      width: "50%",
+                      height: 60,
+                      backgroundColor: color,
+                      border: "2px solid #666",
+                      borderRadius: 8,
+                    }}
+                  />
+                ))}
+              </div>
+            )}
+            <h2>
+              <strong>Gợi ý:</strong> {outfit.outfit}
+            </h2>
+            {outfit.accessories && (
+              <p>
+                <strong>Phụ kiện:</strong> {outfit.accessories}
+              </p>
+            )}
+            {outfit.note && (
+              <p>
+                <em>{outfit.note}</em>
+              </p>
+            )}
+            <ImageGenerator systemPrompt={prompt} />
+          </Card>
+        )
       )}
     </div>
   );
